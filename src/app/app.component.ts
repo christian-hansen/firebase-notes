393f581785f31caf6e5259c0f2275913b58309bf
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-05-12 15:51:58 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection, setDoc, doc } from '@angular/fire/firestore';
+import { deleteDoc } from 'firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-notes';
+  notes$: Observable<any>; // Observable is a variable that updates. Any could also be String, Number etc. but as it is a JSON we use "any". The $ is a mark to identify variables that update
+  // firestore: Firestore = inject(Firestore); // with this we import Firestore
+  notes: Array<any>;
+  notetext = '';
+
+  constructor(private readonly firestore: Firestore) {
+    const itemCollection = collection(this.firestore, 'notes');
+    this.notes$ = collectionData(itemCollection);
+
+    this.notes$.subscribe((newNotes) => {
+      console.log('Notizen sind:', newNotes);
+      this.notes = newNotes;
+    });
+  }
+
+  addNote() {
+    const itemCollection = collection(this.firestore, 'notes');
+    const itemID = 'note' + JSON.stringify(this.notes.length);
+    const note = {title: this.notetext, description: '', id: itemID};
+    setDoc(doc(itemCollection, itemID), note);
+    this.clearInput();
+  }
+
+  async deleteNote(id: string) {  
+    const note = doc(this.firestore, `notes`, `${id}`)
+    await deleteDoc(note);
+  }
+
+  clearInput() {
+    this.notetext = '';
+  }
 }
+
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'notes-84336',
+    appId: '1:1055243060498:web:dfe0fa9452ba3647a25fcb',
+    storageBucket: 'notes-84336.appspot.com',
+    apiKey: 'AIzaSyDzD2G1ZO1_97ldFwzxVPL8rOOyb7IypsA',
+    authDomain: 'notes-84336.firebaseapp.com',
+    messagingSenderId: '1055243060498',
+  },};
